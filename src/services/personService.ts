71e8a962900efbe22672c75699a110acfeb9a890
Ivan Duarte <ivanduarte150199@gmail.com>
2024-10-28 00:41:10 -0300

    bugfix person credits - media type added to movies and tv shows response

--- src/services/personService.ts
+++ src/services/personService.ts
@@ -1,5 +1,6 @@
 import { TimeWindow } from '@appTypes/service/imdb';
 import tmdbApi from './tmdbApi';
+import { MediaType } from '@appTypes/common/MediaType';
 
 export const getTrendingPeople = async (time: TimeWindow = 'week') => {
   try {
@@ -24,7 +25,11 @@ export const getPersonById = async (id: string) => {
 export const getPersonMoviesCredits = async (id: string) => {
   try {
     const response = await tmdbApi.get(`/person/${id}/movie_credits`);
-    return response.data.cast;
+    const updatedCast = response.data.cast.map((item: any) => ({
+      ...item,
+      media_type: MediaType.Movie,
+    }));
+    return updatedCast;
   } catch (error) {
     console.error('Error fetching person movie credits:', error);
     throw error;
@@ -34,21 +39,20 @@ export const getPersonMoviesCredits = async (id: string) => {
 export const getPersonTvCredits = async (id: string) => {
   try {
     const response = await tmdbApi.get(`/person/${id}/tv_credits`);
-    return response.data.cast;
+    const updatedCast = response.data.cast.map((item: any) => ({
+      ...item,
+      media_type: MediaType.Tv,
+    }));
+    return updatedCast;
   } catch (error) {
     console.error('Error fetching person tv credits:', error);
     throw error;
   }
 };
-
 export const getPersonCombinedCredits = async (id: string) => {
   try {
-    const movieCredits = await getPersonMoviesCredits(id);
-    const tvCredits = await getPersonTvCredits(id);
-    return {
-      movies: movieCredits,
-      tv: tvCredits,
-    };
+    const response = await tmdbApi.get(`/person/${id}/combined_credits`);
+    return response.data.cast;
   } catch (error) {
     console.error('Error fetching person combined credits:', error);
     throw error;
